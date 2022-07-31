eb2d3210aad0fed29120ecf0e4ec4f2d24e811eb
G S ASHISH <59803069+Briyani-monster@users.noreply.github.com>
2022-07-31 17:47:34 +0530

    Add files via upload

---
+++ src/com/greatlearning/driver/Main.java
@@ -0,0 +1,16 @@
+package com.greatlearning.driver;
+
+import com.greatlearning.question1.BuildingASkyscrapper;
+import com.greatlearning.question2.TransactionManager;
+
+public class Main {
+	public static void main(String[] args) {
+		//for running building scrapper code 
+//		BuildingASkyscrapper building=new BuildingASkyscrapper();
+//		building.solution();
+		
+//		// for transaction manager
+//		TransactionManager transaction=new TransactionManager();
+//		transaction.solution();
+	}
+}
